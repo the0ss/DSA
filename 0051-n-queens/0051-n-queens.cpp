ad20001758f5b96c7379130205ea5f861f8437cc
Yuvraj Sooraj Chauhan <71978977+the0ss@users.noreply.github.com>
2023-11-30 13:10:16 +0530

    Time: 5 ms (63.68%), Space: 7.6 MB (67.87%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    void solve(int col,vector<string>&board,vector<vector<string>>&ans,vector<int>&left,vector<int>&up,vector<int>&down,int n){
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int r=0;r<n;r++){
+            if(left[r]==0&&up[r+col]==0&&down[n-1+col-r]==0){
+                left[r]=1;
+                up[r+col]=1;
+                down[n-1+col-r]=1;
+                board[r][col]='Q';
+                solve(col+1,board,ans,left,up,down,n);
+                left[r]=0;
+                up[r+col]=0;
+                down[n-1+col-r]=0;
+                board[r][col]='.';
+            }
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++)
+            board[i]=s;
+        vector<int>left(n,0),up(2*n-1,0),down(2*n-1,0);
+        solve(0,board,ans,left,up,down,n);
+        return ans;
+    }
+};
