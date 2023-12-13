92b3bd31b3ae3c28001f097aa1ab963dd540e274
Yuvraj <71978977+the0ss@users.noreply.github.com>
2023-12-13 22:34:29 +0530

    Time: 12 ms (63.92%), Space: 16.1 MB (7.32%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>>pq;
+        // priority_queue<pair<int,int>,vector<pair<int,int>>>pq;
         map<int,int>m;
         for(auto it:nums){
             m[it]++;
         }
+        // for(auto [key,value]:m){
+        //     pq.push({value,key});
+        // }
+         vector<int>ans;
+        // while(k--){
+        //     auto node=pq.top();
+        //     pq.pop();
+        //     ans.push_back(node.second);
+        // }
+        vector<vector<int>>bucket(nums.size()+1);
         for(auto [key,value]:m){
-            pq.push({value,key});
+            bucket[value].push_back(key);
         }
-        vector<int>ans;
-        while(k--){
-            auto node=pq.top();
-            pq.pop();
-            ans.push_back(node.second);
+        for(int i=nums.size();i>=0&&ans.size()<k;i--){
+            if(!bucket[i].empty()){
+                ans.insert(ans.end(),bucket[i].begin(),bucket[i].end());
+            }
         }
         return ans;
     }
