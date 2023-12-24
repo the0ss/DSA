4c5a8a3733f92de4c9f34d78f3073c22468c3c6f
Yuvraj <71978977+the0ss@users.noreply.github.com>
2023-12-24 14:15:00 +0530

    Time: 3 ms (78.34%), Space: 7.4 MB (45.10%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     int minOperations(string s) {
         int n=s.length();
-        string a="",b="";
-        int p=0;
-        for(int i=0;i<n;i++){
-            a+=to_string(p);
-            b+=to_string(!p);
-            p=!p;
-        }
-        int x=0,y=0;
-        for(int i=0;i<n;i++){
-            if(a[i]!=s[i])
-                x++;
-            if(b[i]!=s[i])
-                y++;
-        }
-        return x<y?x:y;
+        // string a="",b="";
+        // int p=0;
+        // for(int i=0;i<n;i++){
+        //     a+=to_string(p);
+        //     b+=to_string(!p);
+        //     p=!p;
+        // }
+        // int x=0,y=0;
+        // for(int i=0;i<n;i++){
+        //     if(a[i]!=s[i])
+        //         x++;
+        //     if(b[i]!=s[i])
+        //         y++;
+        // }
+        // return x<y?x:y;
+        int ans=0;
+        for(int i=0;i<n;i++)
+            if(s[i]-'0'!=i%2)
+                ans++;
+        return min(ans,n-ans);
     }
 };
