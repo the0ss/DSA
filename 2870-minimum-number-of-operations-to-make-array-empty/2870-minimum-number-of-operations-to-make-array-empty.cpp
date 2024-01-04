10b891b4a7f87530ac6ef488ad0eb073057beea6
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-01-04 10:42:09 +0530

    Time: 117 ms (71.64%), Space: 85 MB (90.06%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int ans=0;
-        map<int,int>m;
+        unordered_map<int,int>m;
         for(auto it:nums)
             m[it]++;
         for(auto [key,value]:m){
             if(value==1)
                 return -1;
-            // while(value%3!=0){
-            //     ans++;
-            //     value-=2;
-            // }
-            // ans+=value/3;
-            ans+=value/3+(value%3>0);
+            while(value%3!=0){
+                ans++;
+                value-=2;
+            }
+            ans+=value/3;
         }
         return ans;
     }
