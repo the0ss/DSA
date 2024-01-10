32f73dce4563749caca7478f8c7c88a4df0c8296
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-01-10 13:13:12 +0530

    Time: 620 ms (17.66%), Space: 319.2 MB (5.09%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -12,40 +12,38 @@
 class Solution {
     unordered_map<int,vector<int>> v;  //adjacency list
 
-//create undirected graph for every parent-child  --> DFS
-void createGraph(TreeNode* root,int parent=-1){
-    if(!root) return;
-    int value= root->val;
-    if(parent!=-1){
-        v[parent].push_back(value);
-        v[value].push_back(parent);
+    void createGraph(TreeNode* root,int parent=-1){
+        if(!root) return;
+        int value= root->val;
+        if(parent!=-1){
+            v[parent].push_back(value);
+            v[value].push_back(parent);
+        }
+        createGraph(root->left, value);
+        createGraph(root->right, value);
     }
-    createGraph(root->left, value);
-    createGraph(root->right, value);
-}
 public:
-int amountOfTime(TreeNode* root, int start) {
-    //create graph of given tree
-    createGraph(root);
+    int amountOfTime(TreeNode* root, int start) {
+        //create graph of given tree
+        createGraph(root);
 
-    //start bfs
-    queue<int> q;
-    unordered_map<int,bool> seen;
-    q.push(start);
-    seen[start]=true;
-    int time=0;
-    for(;q.size();time++){
-        int n= q.size();
-        while(n--){
-            auto node= q.front();  q.pop();
-            for(auto i:v[node]){
-                if(!seen[i]){
-                    q.push(i);
-                    seen[i]=true;
+        queue<int> q;
+        unordered_map<int,bool> seen;
+        q.push(start);
+        seen[start]=true;
+        int time=0;
+        for(;q.size();time++){
+            int n= q.size();
+            while(n--){
+                auto node= q.front();  q.pop();
+                for(auto i:v[node]){
+                    if(!seen[i]){
+                        q.push(i);
+                        seen[i]=true;
+                    }
                 }
             }
         }
-    }
-    return time-1;
+        return time-1;
     }
 };
