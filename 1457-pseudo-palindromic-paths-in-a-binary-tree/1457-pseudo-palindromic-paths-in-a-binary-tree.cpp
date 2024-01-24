5a30a5f69a1e30ec73034a5285aeebbec7076c51
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-01-24 10:08:15 +0530

    Time: 235 ms (95.98%), Space: 178.5 MB (96.51%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -10,28 +10,33 @@
  * };
  */
 class Solution {
-    int ans=0;
-    void dfs(TreeNode* root,vector<int>freq){
-        if(!root){
-            return;
-        }
-        freq[root->val-1]++;
-        dfs(root->left,freq);
-        dfs(root->right,freq);
-        if(!root->left&&!root->right){
-            int odds=0;
-            for(auto it:freq)
-                if(it&1)
-                    odds++;
-            if(odds<=1)
-                ans++;
-            return;
-        }
-    }
+    // int ans=0;
+    // void dfs(TreeNode* root,vector<int>freq){
+    //     if(!root){
+    //         return;
+    //     }
+    //     freq[root->val-1]++;
+    //     dfs(root->left,freq);
+    //     dfs(root->right,freq);
+    //     if(!root->left&&!root->right){
+    //         int odds=0;
+    //         for(auto it:freq)
+    //             if(it&1)
+    //                 odds++;
+    //         if(odds<=1)
+    //             ans++;
+    //         return;
+    //     }
+    // }
 public:
-    int pseudoPalindromicPaths (TreeNode* root) {
-        vector<int>freq(9);
-        dfs(root,freq);
-        return ans;
+    int pseudoPalindromicPaths (TreeNode* root,int count=0) {
+        // vector<int>freq(9);
+        // dfs(root,freq);
+        // return ans;
+        if (!root) return 0;
+        count ^= 1 << (root->val - 1);
+        int res = pseudoPalindromicPaths(root->left, count) + pseudoPalindromicPaths(root->right, count);
+        if (root->left == root->right && (count & (count - 1)) == 0) res++;
+        return res;
     }
 };
