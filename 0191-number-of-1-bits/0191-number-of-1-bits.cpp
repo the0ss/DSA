ee04c78cccb8ed8663819b537b1803d196cd04cc
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-01-31 09:15:22 +0530

    Time: 2 ms (39.55%), Space: 7.1 MB (14.85%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,9 +3,8 @@ public:
     int hammingWeight(uint32_t n) {
         int ans=0;
         while(n){
-            if(n&1)
-                ans++;
-            n=n>>1;
+            n=n&(n-1);
+            ans++;
         }
         return ans;
     }
