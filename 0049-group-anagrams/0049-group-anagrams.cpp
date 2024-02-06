678123d1393a7932ece9c2c2265c500273f10392
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-02-06 09:10:13 +0530

    Time: 20 ms (95.19%), Space: 23.3 MB (32.97%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>res;
+        map<string,vector<string>>map;
+        for(auto&i:strs){
+            string s=i;
+            sort(s.begin(),s.end());
+            map[s].push_back(i);
+        }
+        for(auto&i:map){
+            res.push_back(i.second);
+        }
+        return res;
+    }
+};
