79ba0497ab3399331126fcec80839d77c52c936a
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-02-08 23:48:06 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (91.74%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        if(ceil(sqrt(n))==floor(sqrt(n)))
+            return 1;
+        while(n%4==0)
+            n/=4;
+        if(n%8==7)
+            return 4;
+        for(int i=1;i*i<=n;i++){
+            int j=sqrt(n-i*i);
+            if(j*j==n-i*i)
+                return 2;
+        }
+        return 3;
+    }
+};
