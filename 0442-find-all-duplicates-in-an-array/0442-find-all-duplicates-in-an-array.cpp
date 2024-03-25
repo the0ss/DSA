74d62bcc856fc47ccb33e5f4f56ace280926fc4e
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-03-25 11:30:01 +0530

    Time: 44 ms (64.10%), Space: 35.8 MB (90.94%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -5,7 +5,7 @@ public:
         for(int it:nums){
             it=abs(it);
             if(nums[it-1]<0)
-                ans.push_back(abs(it));
+                ans.push_back(it);
             nums[it-1]=-nums[it-1];
         }
         return ans;
