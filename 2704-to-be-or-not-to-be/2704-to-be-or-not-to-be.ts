033b69be84db1364cd1c5c2d88364a86e517f0e1
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-04-03 22:30:30 +0530

    Time: 51 ms (82.79%), Space: 49.8 MB (86.11%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.ts
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.ts
@@ -6,16 +6,14 @@ type ToBeOrNotToBe = {
 function expect(val: any): ToBeOrNotToBe {
     return {
         toBe(val2){
-            if(val === val2){
+            if(val === val2)
                 return true
-           
-            } else throw "Not Equal"
+            throw "Not Equal"
         },
         notToBe(val2){
-            if(val !== val2){
+            if(val !== val2)
                 return true
-           
-            } else throw "Equal"
+            throw "Equal"
         }
     }
 };
