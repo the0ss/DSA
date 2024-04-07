c3608c82e9f75c9d40daa430089f7c40106258a8
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-04-07 15:35:10 +0530

    Time: 7 ms (22.70%), Space: 11.3 MB (79.21%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,29 +21,59 @@ public:
 
 class Solution {
 public:
-    Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
-    {
-        vector<Node*> neighbour;
-        Node* clone=new Node(cur->val);
-        mp[cur]=clone;
-        for(auto it:cur->neighbors){
-            if(mp.find(it)!=mp.end()){
-                neighbour.push_back(mp[it]);
-            }
-            else
-                neighbour.push_back(dfs(it,mp));
-        }
-        clone->neighbors=neighbour;
-        return clone;
-    }
+    // Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
+    // {
+    //     vector<Node*> neighbour;
+    //     Node* clone=new Node(cur->val);
+    //     mp[cur]=clone;
+    //     for(auto it:cur->neighbors){
+    //         if(mp.find(it)!=mp.end()){
+    //             neighbour.push_back(mp[it]);
+    //         }
+    //         else
+    //             neighbour.push_back(dfs(it,mp));
+    //     }
+    //     clone->neighbors=neighbour;
+    //     return clone;
+    // }
     Node* cloneGraph(Node* node) {
-        unordered_map<Node*,Node*> mp;
+        // unordered_map<Node*,Node*> mp;
+        // if(node==NULL)
+        //     return NULL;
+        // if(node->neighbors.size()==0){
+        //     Node* clone= new Node(node->val);
+        //     return clone; 
+        // }
+        // return dfs(node,mp);
         if(node==NULL)
-            return NULL;
-        if(node->neighbors.size()==0){
-            Node* clone= new Node(node->val);
-            return clone; 
+        return NULL;
+        queue<Node*> q;
+        int visited[105];
+        memset(visited, 0, sizeof(visited));
+        Node* root_node = new Node(node->val);
+        q.push(node);
+        visited[node->val]=1;
+
+        map<Node*, Node*> mp;
+        mp[node]= root_node;
+       
+        while(!q.empty())
+        {
+            Node* temp= q.front();
+            q.pop();
+            
+            for(auto child: temp->neighbors)
+            {
+                if(visited[child->val]==0)
+                {
+                    Node *new_node= new Node(child->val);
+                    visited[child->val]=1;
+                    mp[child]=new_node;
+                    q.push(child);
+                }
+                mp[temp]->neighbors.push_back(mp[child]);
+            }
         }
-        return dfs(node,mp);
+        return root_node;
     }
 };
