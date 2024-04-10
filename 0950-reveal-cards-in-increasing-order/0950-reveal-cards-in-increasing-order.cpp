409e53559188f92149a4d6e3f902e7256d3b7828
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-04-10 11:47:58 +0530

    Time: 6 ms (30.99%), Space: 11.8 MB (5.76%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(begin(deck),end(deck));
+        reverse(begin(deck),end(deck));
+        int n=deck.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            int last=-1;
+            if(!ans.empty()){
+                last=ans.back();
+                ans.pop_back();
+            }
+            ans.insert(begin(ans),deck[i]);
+            if(last!=-1){
+                if(i==n-1){
+                    ans.push_back(last);
+                    break;
+                }
+                ans.insert(begin(ans),last);
+            }
+        }
+        return ans;
+    }
+};
