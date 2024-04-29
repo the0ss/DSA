7367ac0011c64609a84c8144c4b86de565ac67bb
Yuvraj <71978977+the0ss@users.noreply.github.com>
2024-04-29 11:38:11 +0530

    Time: 88 ms (97.03%), Space: 91.3 MB (85.86%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int ans=0;
-        int xoor=0;
+        // int ans=0;
+        // int xoor=0;
+        // for(auto it:nums)
+        //     xoor^=it;
+        // if(xoor==k)
+        //     return 0;
+        // bitset<32> x(xoor),y(k);
+        // string a=x.to_string();
+        // string b=y.to_string();
+        // for(int i=0;i<a.size();i++){
+        //     if(a[i]!=b[i])
+        //         ans++;
+        // }
+        // return ans;
+        
         for(auto it:nums)
-            xoor^=it;
-        if(xoor==k)
-            return 0;
-        bitset<32> x(xoor),y(k);
-        string a=x.to_string();
-        string b=y.to_string();
-        for(int i=0;i<a.size();i++){
-            if(a[i]!=b[i])
-                ans++;
-        }
-        return ans;
+            k^=it;
+        return __builtin_popcount(k);
     }
 };
